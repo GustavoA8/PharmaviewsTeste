06c4a2737f81f3291eef0d3670ee242f0d4c4f67
GustavoA8 <fefe.giulia2@gmail.com>
2025-03-19 14:57:53 -0300

    html quase pronto

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+const tabela = document.getElementById("tabela-corpo")
+console.log("Chamando js")
+
+function adicionarAcao(){
+    console.log("chamando addacao")
+    tabela.innerHTML += `
+    <tr class="table-active">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td><img src="" alt=""></td>
+        <td><img src="" alt=""></td>
+    </tr>
+    
+
+    
+    `
+    event.preventDefault();
+    
+}
+function limpar(){
+    const input = document.querySelectorAll("input") 
+    console.log(input)
+
+    input[0].value = "";
+    input[1].value = "";
+    input[2].value = "";
+
+    // for( input in i){
+    //     input[i].value = "";
+        
+    // }
+
+    event.preventDefault();
+}
